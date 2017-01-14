d3d836a07e17c3e4a4b20373d5af9624222ffe17
Lucier Lima <lucierflima@gmail.com>
2017-01-14 01:14:43 -0300

    teste 3

--- portal/settings.py
+++ portal/settings.py
@@ -16,6 +16,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+import dj_database_url
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
